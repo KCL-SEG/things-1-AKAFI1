053e432cf222079e401fdab79a2a43ef28619151
Kafi <ahmed.kafi@outlook.com>
2022-12-08 14:32:57 +0000

    exercise 1

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     quantity = models.IntegerField()
 
 
